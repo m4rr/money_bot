9f899fc9bd85c23b71a0d0a446ac46452182f8a3
Marat S <remarr@gmail.com>
2015-12-31 13:39:28 +0300

    currency translate

--- bot.rb
+++ bot.rb
@@ -7,23 +7,42 @@ load "token.rb"
 #   exit 130
 # end
 
-def parse_values values
-  return {} if values.is_a?(Fixnum)
-  puts values
-  # hash = {}
-  values.each do |e|
-    case e
-    when '$'
-      hash[:currency] = :USD
-    when '€'
-      hash[:currency] = :EUR
-    when '₽'
-      hash[:currency] = :RUB
-    when /(\d+)/
-      hash[:amount] = $1
-    end
+@base_rub_json
+
+def detect_currency value
+  case value
+  when '$'
+    :USD
+  when '€'
+    :EUR
+  when '₽'
+    :RUB
   end
-  hash
+end
+
+def convert hash
+  amount = hash[:amount]
+  currency = detect_currency hash[:currency]
+
+  if @base_rub_json.nil?
+    response = Net::HTTP.get_response('api.fixer.io', '/latest?base=RUB')
+    @base_rub_json = JSON.parse response.body
+  end
+
+  if currency == :RUB
+    to_cur = :USD
+  elsif currency == :USD || currency == :EUR
+    to_cur = :RUB
+  end
+
+  val = @base_rub_json['rates'][:USD.to_s] if currency == :USD || currency == :RUB
+  val = @base_rub_json['rates'][:EUR.to_s] if currency == :EUR
+
+  if currency == :RUB
+    "#{amount.to_f * val.to_f} of #{to_cur}"
+  else
+    "#{amount.to_f / val.to_f} of #{to_cur}"
+  end
 end
 
 Telegram::Bot::Client.run(TOKEN) do |bot|
@@ -33,9 +52,9 @@ Telegram::Bot::Client.run(TOKEN) do |bot|
       amount = $2
       currency = [$1, $3].compact.first
       hash = {amount: amount, currency: currency}
-      bot.api.send_message(chat_id: message.chat.id, text: "got it #{hash.to_s}")
+      bot.api.send_message(chat_id: message.chat.id, text: "#{convert hash} ")
     when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}. Ask me '$4'.")
     when '/stop'
       bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
     end
