3b5727ee72ce4ec857c0b5ef02383e114bb5adb1
Marat Saytakov <remarr@gmail.com>
2019-02-09 10:35:03 +0700

    testes

--- money_bot/bot.rb
+++ money_bot/bot.rb
@@ -80,7 +80,7 @@ def parse_message message
     "https://bipbap.ru/wp-content/uploads/2017/08/1473780855_29.jpg",
   ]
 
-  if message.chat.title == "тест-марат-ираклий" || message.from.username == "pearl_hush"
+  if message.chat.title == "тест-марат-ираклий" && message.from.username == "maratacrobat" || message.from.username == "pearl_hush"
     rmax = happy_bday.count * 1
     rnum = Random.rand(0..rmax)
     if rnum < happy_bday.count
